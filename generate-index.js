a870053b00abbdbd0713a9327f60326aba59e7fd
Rafael Bomfim <ra-bonfim@hotmail.com>
2024-11-25 13:20:49 -0300

    Retirando quebra proposital

--- generate-index.js
+++ generate-index.js
@@ -195,7 +195,6 @@ async function updateSheetLinks(sheets, sheetName, links) {
 
 // Função principal
 async function main() {
-    process.exit(1);
     const sheets = google.sheets({ version: 'v4', auth });
 
     const readSheetData = async (range) => {
